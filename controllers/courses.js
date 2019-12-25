956b7fc4ec60f96e49f9c7f9e88a81a0390deeb4
ando <dokyan1989@gmail.com>
2019-12-25 16:55:26 +0700

    remove getBootcampsInRadius from course controller

--- controllers/courses.js
+++ controllers/courses.js
@@ -9,7 +9,9 @@ const Bootcamp = require('../models/Bootcamp');
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
   if (req.params.bootcampId) {
-    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+    const courses = await Course.find({
+      bootcamp: req.params.bootcampId
+    });
 
     return res.status(200).json({
       success: true,
@@ -36,7 +38,10 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
     );
   }
 
-  res.status(200).json({ success: true, data: course });
+  res.status(200).json({
+    success: true,
+    data: course
+  });
 });
 
 // @desc    Add course
@@ -100,7 +105,10 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
     runValidators: true
   });
 
-  res.status(200).json({ success: true, data: course });
+  res.status(200).json({
+    success: true,
+    data: course
+  });
 });
 
 // @desc    Delete course
@@ -127,32 +135,8 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
 
   await course.remove();
 
-  res.status(200).json({ success: true, data: {} });
-});
-
-// @desc    Get bootcamps within a radius
-// @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
-// @access  Private
-exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
-  const { zipcode, distance } = req.params;
-
-  // Get lat/lng from geocoder
-  const loc = await geocoder.geocode(zipcode);
-  const lat = loc[0].latitude;
-  const lng = loc[0].longitude;
-
-  // Calc radius using radians
-  // Divide distance by radius of Earth
-  // Earth Radius = 3,963 miles / 6,378 km
-  const radius = distance / 3963;
-
-  const bootcamps = await Bootcamp.find({
-    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
-  });
-
   res.status(200).json({
     success: true,
-    count: bootcamps.length,
-    data: bootcamps
+    data: {}
   });
 });
